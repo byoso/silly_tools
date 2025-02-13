d272c77ac5388dc3890ee341e6fc031e22e66d1d
byoso <emailvinz@gmail.com>
2025-02-13 18:30:54 +0100

    removed silly_engine to give it a proper repo

--- silly_engine/__init__.py
+++
@@ -1,5 +0,0 @@
-from .router import Router, Subrouter, RouterError
-from .jsondb import JsonDB, JsonDBError
-from .text_tools import Color as c, Title, print_title
-from .logger import Logger
-from .minuit import Field, Form, ListField, FieldError, FormError, Confirmation, Menu, clear, AutoArray, TextField, print_formated
